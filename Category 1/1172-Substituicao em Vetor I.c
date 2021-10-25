6879ad030a21e36b18f581d59b1b360a3f1cb422
LuizH-Queiroz <luizh.keiroz@gmail.com>
2021-10-25 12:09:58 -0300

    BEECROWD1172

---
+++ Category 1/1172-Substituicao em Vetor I.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define AR_SIZE 10 //Tamanho do array
+
+
+int main(){
+    int ar[AR_SIZE];
+    int i;
+
+    for (i = 0; i < AR_SIZE; i++)
+    {
+        scanf("%d", &ar[i]);
+        if (ar[i] <= 0){
+            ar[i] = 1;
+        }
+
+        printf("X[%d] = %d\n", i, ar[i]);
+    }
+
+
+    return 0;
+}
